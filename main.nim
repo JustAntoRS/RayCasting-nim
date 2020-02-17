4eb051ae1e31a46d33ecafb6d0e1d2b02bb39b0b
JustAntoRS <justantorami@gmail.com>
2020-02-17 22:35:13 +0100

    DDA

--- main.nim
+++ main.nim
@@ -2,6 +2,19 @@ import sdl2
 
 # ------ SDL2 CONF ------
 
+discard sdl2.init(INIT_EVERYTHING)
+
+var
+  window : WindowPtr
+  render : RendererPtr
+
+window = createWindow("NIM RayCasting", 100,100,640,480, SDL_WINDOW_SHOWN)
+render = createRenderer(window, -1, Renderer_Accelerated or Renderer_PresentVsync or Renderer_TargetTexture)
+
+
+var
+  evt = sdl2.defaultEvent
+  runGame = true
 
 # ------ GAME DATA ------
 
@@ -31,3 +44,71 @@ var worldMap =[
   [1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
   [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
 ]
+
+var
+  posX : float = 22
+  posY : float = 12
+  dirX : float = -1
+  diry : float =  0
+  planeX : float = 0
+  planeY : float = 0.66
+  time : float = 0
+  oldTime : float = 0
+
+# ------ MAIN LOOP ------
+
+while runGame:
+  while pollEvent(evt):
+    if evt.kind == QuitEvent:
+      runGame = false
+      break
+
+  for x in 0..24:
+    var
+      cameraX : float = 2.0 * float(x) / float(24) - 1
+      rayDirX : float = dirX + planeX * cameraX
+      rayDirY : float = dirY + planeY * cameraX
+      mapX : int = int(posX)
+      mapY : int = int(posY)
+      sideDistX : float
+      sideDistY : float
+      deltaDistX : float = abs(1 / rayDirX)
+      deltaDistY : float = abs(1 / rayDirY)
+      perpWallDist : float
+      stepX : int
+      stepY : int
+      hit : int = 0
+      side : int
+
+    if rayDirX < 0:
+      stepX = -1
+      sideDistX = (posX - float(mapX) * deltaDistX)
+    else:
+      stepX = 1
+      sideDistX = (float(mapX) + 1.0 - posX) * deltaDistX
+
+    if rayDirY < 0:
+      stepY = -1
+      sideDistY = (posY - float(mapY)) * deltaDistY
+    else:
+      stepY = 1
+      sideDistY = (float(mapY) + 1.0 - posY) * deltaDistY
+
+    while hit == 0:
+      if sideDistX < sideDistY:
+        sideDistX += deltaDistX
+        mapX += stepX
+        side = 0
+      else:
+        sideDistY += deltaDistY
+        mapY += stepY
+        side = 1
+
+      if worldMap[mapX][mapY] > 0 : hit = 1
+
+  render.setDrawColor(0,0,0,255)
+  render.clear
+  render.present
+
+destroy render
+destroy window
